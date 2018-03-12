c5d8eaf6cadd4ff0aaf0d20106457db5f2df577a
Armand Kapllani <akapllani@users.noreply.github.com>
2018-03-12 00:23:36 -0400

    Update

--- TestCode.R
+++ TestCode.R
@@ -4,3 +4,11 @@
 x <- runif(1000, min = 0, max = 1)
 y <- rnorm(1000, mean = 0, sd = 10)
 plot(x,y)
+
+# Regression example 
+
+epsilon <- rnorm(1000, 0, 1)
+y <- x + epsilon
+
+reg <- lm(y ~ x)
+summary(reg)
